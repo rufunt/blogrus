6951bad0202c1d9a9f6becbb4a3bf6898de5d007
rufunt <jazzoo@mail.ru>
2018-03-11 23:25:37 +0300

    Add show and article_params methods and  show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,20 @@ class ArticlesController < ApplicationController
     
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
   def create
-    @article = Article.new(params[:article])
+    @article = Article.new(article_params)
  
     @article.save
     redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
