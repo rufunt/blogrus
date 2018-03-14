a5f5bb3d339d03413e6a4dc245d7aeb1f8b64967
rufunt <jazzoo@mail.ru>
2018-03-14 21:16:50 +0300

    Add validates to new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def show
@@ -15,8 +16,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
  
-    @article.save
+  if @article.save
     redirect_to @article
+  else
+    render 'new'
+  end
   end
 
   private
